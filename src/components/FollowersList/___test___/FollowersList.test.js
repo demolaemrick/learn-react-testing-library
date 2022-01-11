a5a5d48b4464739241059afc9e1c50d4688198bb
Emrick19 <emricky17@gmail.com>
2022-01-11 18:18:03 +0100

    Finish testing

---
+++ src/components/FollowersList/___test___/FollowersList.test.js
@@ -0,0 +1,20 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FollowersList from "../FollowersList";
+
+const MockFollowersList = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+
+describe("FollowersList", () => {
+  it("should fetch and render input element", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    screen.debug();
+    expect(followerDivElement).toBeInTheDocument();
+  });
+});
--- src/components/Header/___test___/Header.test.js
+++ src/components/Header/___test___/Header.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
-it("should render the same text render into title prop", () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.getByText(/my header/i);
+describe("Header", () => {
+  it("should render the same text render into title prop", () => {
+    render(<Header title="My Header" />);
+    const headingElement = screen.getByText(/my header/i);
 
-  expect(headingElement).toBeInTheDocument();
+    expect(headingElement).toBeInTheDocument();
+  });
 });
